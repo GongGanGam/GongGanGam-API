3a689e5579e780315c9c2cd7f0557fce46eb4184
SY-Mina <sylim9387@naver.com>
2022-03-12 00:00:23 +0900

    Fix Kakao Login Logic

--- src/app/User/userController.js
+++ src/app/User/userController.js
@@ -28,98 +28,60 @@ exports.getTest = async function (req, res) {
  */
 exports.loginKakao = async function (req, res) {
 
-    var code = req.query.code;
-    const client_id = kakao.client_id;
-    const client_secret = kakao.client_secret;
-    console.log('code: '+code);
+    const { token } = req.body;
+    var header = 'Bearer ' + token; // Bearer 다음에 공백 추가
+    console.log('token ' + token);
 
-    let api_url = 'https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id='
-        + client_id + '&redirect_uri=http://3.36.219.12:3000/app/users/login/kakao&code='+code
-        +'&client_secret='+client_secret;
-    var request = require('request');
+    var api_url = 'https://kapi.kakao.com/v2/user/me';
+    var request2 = require('request');
+    console.log(body);
+
     var options = {
         url: api_url,
-        headers: {
-            'Content-Type':'application/x-www-form-urlencoded;'
-        },
-        /*body : {
-            'grant_type' : 'authorization_code',
-            'client_id' : client_id,
-            'redirect_uri' : 'http://3.34.199.201:3000/app/users/login/kakao',
-            'code' : code,
-            'client_secret' : client_secret
-        }*/
+        headers: {'Authorization': header},
     };
-    console.log('aaaaaa');
-    request.post(options, function (error, resp, body) {
-        if (!error) {
-            console.log(body);
-            console.log('success token');
-            const obj = JSON.parse(body);
-            var token = obj.access_token;
-            var header = 'Bearer ' + token; // Bearer 다음에 공백 추가
-            console.log('token ' + token);
+    request2.get(options, async function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log('success me');
+            const myInfo = JSON.parse(body);
+            console.log(myInfo)
+            const email = myInfo.kakao_account.email;
+            const identification = myInfo.id;
+            console.log(email);
+            console.log('id: ' + identification)
 
-            var api_url = 'https://kapi.kakao.com/v2/user/me';
-            var request2 = require('request');
-            console.log(body);
+            // DB에 유저 있는지 확인 후, 없으면 로그인 처리
+            const userExist = await userProvider.checkUserExist(email, identification);
+            console.log(userExist)
+            if (userExist.length > 0) {
+                const signInResponse = await userService.postKaKaoLogin(identification);
+                return res.send(signInResponse);
+            }
+            // 회원가입하게 받은 정보 리턴해주기.
+            else {
 
-            var options = {
-                url: api_url,
-                headers: {'Authorization': header},
-            };
-            request2.get(options, async function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    console.log('success me');
-                    const myInfo = JSON.parse(body);
-                    console.log(myInfo)
-                    const email = myInfo.kakao_account.email;
-                    const identification = myInfo.id;
-                    console.log(email);
-                    console.log('id: ' + identification)
+                const signinResponse = await userService.createUser("nickname", 2000, "N", "kakao", email, identification);
 
-                    // DB에 유저 있는지 확인 후, 없으면 로그인 처리
-                    const userExist = await userProvider.checkUserExist(email, identification);
-                    console.log(userExist)
-                    if (userExist.length>0) {
-                        const signInResponse = await userService.postKaKaoLogin(identification);
-                        return res.send(signInResponse);
-                    }
-                    // 회원가입하게 받은 정보 리턴해주기.
-                    else {
-                        let nickname='', birthYear='', gender='';
-                        if (myInfo.name) nickname = myInfo.name;
-                        if (myInfo.gender) gender = myInfo.gender;
-                        if (myInfo.birthYear) birthYear = myInfo.birthYear;
+                return res.json({
+                    isSuccess: false,
+                    code: 5028,
+                    message: "로그인 실패. 회원가입해주세요",
+                    result: signinResponse
+                });
 
-                        const result = {'nickname' : nickname, 'birthYear' : birthYear, 'gender' : gender, 'type' : 'kakao',
-                            'email' : email, 'identification' : identification}
+            }
 
-                        return res.json({
-                            isSuccess: false,
-                            code     : 5028,
-                            message  : "로그인 실패. 회원가입해주세요",
-                            result   : result
-                        });
-                    }
-
-                } else {
-                    console.log('error');
-                    if(response != null) {
-                        //res.status(response.statusCode).end();
-                        console.log('me error = ' + response.statusCode);
-                        return res.send(errResponse(baseResponse.LOGIN_KAKAO_TOKEN_ERROR));
-                    }
-                    return res.send(errResponse(baseResponse.LOGIN_KAKAO_ERROR));
-                }
-            });
         } else {
-
-            console.log('token error = ' + response.statusCode);
-            return res.send(errResponse(baseResponse.LOGIN_KAKAO_TOKEN_ERROR));
-
+            console.log('error');
+            if (response != null) {
+                //res.status(response.statusCode).end();
+                console.log('me error = ' + response.statusCode);
+                return res.send(errResponse(baseResponse.LOGIN_KAKAO_TOKEN_ERROR));
+            }
+            return res.send(errResponse(baseResponse.LOGIN_KAKAO_ERROR));
         }
     });
+
 }
 
 
